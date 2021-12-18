4d517b71c9f0ecde353f7b09bb270c0e5388a635
Matthew Mackey <21043873+matthewmackey@users.noreply.github.com>
2021-12-17 23:25:27 -0500

    kubectl: add completion install section from oh-my-zsh

--- plugins/kubectl/common.sh
+++ plugins/kubectl/common.sh
@@ -1,5 +1,24 @@
 #-------------------------------------------------------------------------------
-# [Kubernetes] {{{
+# [completion] {{{
+#-------------------------------------------------------------------------------
+# From oh-my-zsh kubectl plugin
+if (( $+commands[kubectl] )); then
+  __KUBECTL_COMPLETION_FILE="${ZSH_CACHE_DIR}/kubectl_completion"
+
+  if [[ ! -f $__KUBECTL_COMPLETION_FILE || ! -s $__KUBECTL_COMPLETION_FILE ]]; then
+    kubectl completion zsh >! $__KUBECTL_COMPLETION_FILE
+  fi
+
+  [[ -f $__KUBECTL_COMPLETION_FILE ]] && source $__KUBECTL_COMPLETION_FILE
+
+  unset __KUBECTL_COMPLETION_FILE
+fi
+
+# }}}
+
+
+#-------------------------------------------------------------------------------
+# [aliases]] {{{
 #-------------------------------------------------------------------------------
   alias kgctx="kubectl config get-contexts"
   alias krctx="kubectl config rename-context"
