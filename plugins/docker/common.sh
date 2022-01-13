0fc508054335579532427f4300e3df1d15488a3b
Matthew Machaj <21043873+matthewmackey@users.noreply.github.com>
2022-01-13 18:12:09 -0500

    Add 'docker network'; fixed compose aliases; fix fzf passing args issue

--- plugins/docker/common.sh
+++ plugins/docker/common.sh
@@ -1,5 +1,5 @@
 #------------------------------------------------------------------------------#
-# [docker:aliases:sh] {{{                                                      #
+# [docker:aliases:sh] {{{                              =                        #
 #------------------------------------------------------------------------------#
   # attach
   # alias da="docker attach -it"
@@ -23,8 +23,7 @@
   alias dimp="docker import"
 
   # info/inspect
-  alias dif="docker info"
-  alias din="docker inspect"
+  alias dinfo="docker info"
 
   # load
   alias dlo="docker load"
@@ -32,6 +31,19 @@
   # login/logout
   alias dlgn="docker login"
   alias dlgt="docker logout"
+
+  # logs
+  # SEE fzf functions below
+
+  # network
+  alias dn="docker network"
+  alias dnco="docker network connect"
+  alias dncr="docker network create"
+  alias dnd="docker network disconnect"
+  alias dni="docker network inspect"
+  alias dnl="docker network ls"
+  alias dnp="docker network prune"
+  alias dnr="docker network rm"
 
   # ps
   alias dp="cl; docker ps"
@@ -69,7 +81,18 @@ then
     local _cid
     _cid=$(docker ps | sed 1d | fzf | awk '{print $1}')
 
-    [ -n "$_cid" ] && docker $_cmd "$_cid"
+    [ -n "$_cid" ] && docker $_cmd ${_args[@]} "$_cid"
+  }
+
+  __docker_ps_all() {
+    local _cmd=$1
+    shift
+    local _args=$@
+
+    local _cid
+    _cid=$(docker ps -a | sed 1d | fzf | awk '{print $1}')
+
+    [ -n "$_cid" ] && docker $_cmd ${_args[@]} "$_cid"
   }
 
   __docker_ps_multi() {
@@ -78,9 +101,20 @@ then
     local _args=$@
 
     local _cids
-    _cids=$(docker ps | sed 1d | fzf --multi | awk '{print $1}')
+    _cids=($(docker ps | sed 1d | fzf --multi | awk '{print $1}'))
 
-    [ -n "$_cids" ] && docker $_cmd "$_cids"
+    [ -n "$_cids" ] && docker $_cmd ${_args[@]} $_cids
+  }
+
+  __docker_ps_all_multi() {
+    local _cmd=$1
+    shift
+    local _args=$@
+
+    local _cids
+    _cids=($(docker ps -a | sed 1d | fzf --multi | awk '{print $1}' | tr '\n' ' '))
+
+    [ -n "$_cids" ] && docker $_cmd ${_args[@]} $_cids
   }
 
   __docker_images() {
@@ -91,7 +125,7 @@ then
     local _cid
     _cid=$(docker images | sed 1d | fzf | awk '{print $3}')
 
-    [ -n "$_cid" ] && docker $_cmd "$_cid"
+    [ -n "$_cid" ] && docker $_cmd ${_args[@]} "$_cid"
   }
 
   __docker_images_multi() {
@@ -100,26 +134,31 @@ then
     local _args=$@
 
     local _cids
-    _cids=$(docker images | sed 1d | fzf | awk '{print $3}')
+    _cids=($(docker images | sed 1d | fzf | awk '{print $3}'))
 
-    [ -n "$_cids" ] && docker $_cmd "$_cids"
+    [ -n "$_cids" ] && docker $_cmd ${_args[@]} "$_cids"
   }
 
-  # single-selection
+  # PS: single-selection
   da()    { __docker_ps attach $@ ; }
-  dco()   { __docker commit $@ ; }
-  ddi()   { __docker diff $@ ; }
-  dexp()  { __docker export $@ ; }
-  dlogs() { __docker_ps logs $@ ; }
+  dco()   { __docker_ps commit $@ ; }
+  ddi()   { __docker_ps diff $@ ; }
+  dexp()  { __docker_ps export $@ ; }
+  dins()  { __docker_ps inspect $@ ; }
+  dl()    { __docker_ps logs $@ ; }
+  dlf()   { __docker_ps logs -f $@ ; }
+  dla()   { __docker_ps_all logs $@ ; }
+  dlaf()  { __docker_ps_all logs -f $@ ; }
   dprt()  { __docker_ps port $@ ; }
   deti()  { __docker_ps exec -it $@ ; }
   drn()   { __docker_ps rename $@ ; }
   dtop()  { __docker_ps top $@ ; }
 
-  # multi-selection
+  # PS: multi-selection
   dk()   { __docker_ps_multi kill $@ ; }
   drst() { __docker_ps_multi restart $@ ; }
   drm()  { __docker_ps_multi rm $@ ; }
+  drma() { __docker_ps_all_multi rm $@ ; }
   dpau() { __docker_ps_multi pause $@ ; }
   dst()  { __docker_ps_multi start $@ ; }
   dstp() { __docker_ps_multi stop $@ ; }
@@ -128,12 +167,12 @@ then
   dw()   { __docker_ps_multi wait $@ ; }
 
 
-  # single-selection
+  # Images: single-selection
   dh()    { __docker_images history $@ ; }
   dpush() { __docker_images push $@ ; }
   dtag()  { __docker_images tag $@ ; }
 
-  # multi-selection
+  # Images: multi-selection
   dsa()   { __docker_images_multi save $@ ; }
   drmi()  { __docker_images_multi rmi $@ ; }
 fi
@@ -150,9 +189,9 @@ fi
   alias dcu="docker-compose up"
   alias dcud="docker-compose up -d"
 
-  alias dcrm"docker-compose rm"
-  alias dcrms"docker-compose rm -s"
-  alias dcrmsf"docker-compose rm -s -f"
+  alias dcrm="docker-compose rm"
+  alias dcrms="docker-compose rm -s"
+  alias dcrmsf="docker-compose rm -s -f"
 # }}} docker-compose:aliases
 
 # vim: set ft=sh foldmarker={{{,}}} foldmethod=marker foldlevel=0 :
