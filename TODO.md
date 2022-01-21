dd6636ac824736530aed1e84266d0fb7b8e6da83
Matthew Machaj <21043873+matthewmackey@users.noreply.github.com>
2022-01-21 15:07:29 -0500

    systemctl: add systemctl alias plugin

---
+++ TODO.md
@@ -0,0 +1,5 @@
+newmain is the correct line of commits.  I need to get rid of the original
+'origin/main' branch and make 'origin/newmain' be 'origin/main'.
+
+The main point of this is to ensure that the last 2x commits on origin/main
+are removed from the remote repo b/c they have my DL email address in them.
---
+++ plugins/systemctl/common.sh
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# [aliases]] {{{
+#-------------------------------------------------------------------------------
+
+  # Systemctl
+  alias sc="sudo systemctl"
+  alias scs="sudo systemctl status"
+  alias scr="sudo systemctl restart"
+  alias scstart="sudo systemctl start"
+  alias scstop="sudo systemctl stop"
+
+  # Journalctl
+  alias jc="sudo journalctl -u"
+  alias jcf="sudo journalctl -f -u"
+
+# }}}  aliases
+
+
+# vim: set ft=sh foldmarker={{{,}}} foldmethod=marker foldlevel=100 :
