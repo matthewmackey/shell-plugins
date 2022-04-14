bfd36bf82202f833e29fff1b6b37674e30c38c3f
Matthew Machaj <21043873+matthewmackey@users.noreply.github.com>
2022-04-14 14:31:52 -0400

    ekubeconfig: add func to manage Kubeconfig files

--- plugins/kubectl/common.sh
+++ plugins/kubectl/common.sh
@@ -1,4 +1,52 @@
 #-------------------------------------------------------------------------------
+# [Kubeconfig File Management] {{{
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------
+# ekubeconfig
+#
+# Updates your ~/.kube/config symlink to the provide kubeconfig file
+# so that your `kubectl` commands will apply to the same cluster
+# across all of your shell windows.
+#
+# OPTIONS
+#
+#  -e  if you provide this as the 2nd argument to this command
+#      then it will only set the KUBECONFIG environment variable
+#      in your current shell so that your `kubectl` commands will
+#      only run against the cluster you specify in the shell only
+#-------------------------------------------------------------------
+ekubeconfig() {
+  local kubeconfig_path=$1
+  local set_kubeconfig_only=${2:-NO}
+
+  if [ -z "$kubeconfig_path" ] || [ ! -f $kubeconfig_path -a ! -L $kubeconfig_path ]; then
+    printf "You must provide a valid path\n"
+    return 1
+  fi
+
+  if [ "$set_kubeconfig_only" != "-e" ]; then
+    if [ -e ~/.kube/config -a ! -L ~/.kube/config ]; then
+      printf "Cannot proceed becuase ~/.kube/config is not a symlink and we can't delete it\n"
+      return 1
+    fi
+
+    printf "Setting ~/.kube/config -> $kubeconfig_path\n"
+    if [ -e ~/.kube/config ]; then
+      rm ~/.kube/config
+    fi
+
+    ln -s $kubeconfig_path ~/.kube/config
+  else
+    printf "Setting KUBECONFIG=$kubeconfig_path\n"
+    export KUBECONFIG=$kubeconfig_path
+  fi
+}
+
+# }}}
+
+
+#-------------------------------------------------------------------------------
 # [completion] {{{
 #-------------------------------------------------------------------------------
 # From oh-my-zsh kubectl plugin
@@ -16,54 +64,55 @@ fi
 
 # }}}
 
-create_std_k8s_aliases() {
-  local _abbrev=$1
-  local _resource=$2
-
-  # kg_
-  alias kg${_abbrev}="kubectl get $_resource"
-
-  # kg_l
-  alias kg${_abbrev}l="kubectl get $_resource --show-labels"
-
-  # kg_a
-  alias kg${_abbrev}a="kubectl get $_resource -A"
-
-  # kg_al
-  alias kg${_abbrev}al="kubectl get $_resource -A --show-labels"
-
-  # kg_w
-  alias kg${_abbrev}w="kubectl get $_resource -o wide"
-
-  # kg_wl
-  alias kg${_abbrev}wl="kubectl get $_resource -o wide --show-labels"
-
-  # kg_wa
-  alias kg${_abbrev}wa="kubectl get $_resource -o wide -A"
-
-  # kg_wal
-  alias kg${_abbrev}wal="kubectl get $_resource -o wide -A --show-labels"
-
-  # kg_watch
-  alias kg${_abbrev}watch="kubectl get $_resource --watch"
-
-  # kd_
-  alias kd${_abbrev}="kubectl describe $_resource"
-
-  # kdel_
-  alias kdel${_abbrev}="kubectl delete $_resource"
-
-  # kdel_l
-  alias kdel${_abbrev}l="kubectl delete $_resource -l"
-
-  # ke_
-  alias ke${_abbrev}="kubectl edit $_resource"
-
-}
 
 #-------------------------------------------------------------------------------
 # [aliases]] {{{
 #-------------------------------------------------------------------------------
+  create_std_k8s_aliases() {
+    local _abbrev=$1
+    local _resource=$2
+
+    # kg_
+    alias kg${_abbrev}="kubectl get $_resource"
+
+    # kg_l
+    alias kg${_abbrev}l="kubectl get $_resource --show-labels"
+
+    # kg_a
+    alias kg${_abbrev}a="kubectl get $_resource -A"
+
+    # kg_al
+    alias kg${_abbrev}al="kubectl get $_resource -A --show-labels"
+
+    # kg_w
+    alias kg${_abbrev}w="kubectl get $_resource -o wide"
+
+    # kg_wl
+    alias kg${_abbrev}wl="kubectl get $_resource -o wide --show-labels"
+
+    # kg_wa
+    alias kg${_abbrev}wa="kubectl get $_resource -o wide -A"
+
+    # kg_wal
+    alias kg${_abbrev}wal="kubectl get $_resource -o wide -A --show-labels"
+
+    # kg_watch
+    alias kg${_abbrev}watch="kubectl get $_resource --watch"
+
+    # kd_
+    alias kd${_abbrev}="kubectl describe $_resource"
+
+    # kdel_
+    alias kdel${_abbrev}="kubectl delete $_resource"
+
+    # kdel_l
+    alias kdel${_abbrev}l="kubectl delete $_resource -l"
+
+    # ke_
+    alias ke${_abbrev}="kubectl edit $_resource"
+
+  }
+
   # General
   alias k="kubectl"
   alias kd="kubectl describe"
@@ -141,6 +190,10 @@ create_std_k8s_aliases() {
   alias klf1m="kubectl logs --since 1m -f"
   alias klf1s="kubectl logs --since 1s -f"
 
+  alias klin="kubectl logs -l app.kubernetes.io/component=controller,app.kubernetes.io/instance=ingress-nginx,app.kubernetes.io/name=ingress-nginx"
+  alias klinf="klin -f"
+  alias klinft="klin -f --tail=-1"
+
   # Rollout
   alias krr="kubectl rollout restart"
   alias krrds="kubectl rollout restart daemonset"
@@ -183,7 +236,12 @@ create_std_k8s_aliases() {
 
   # EXTRA
   alias kgps="kubectl get pods --selector"
-  alias kgpsl="kubectl get pods --show-labels"
+
+  alias kgpsort="kgp --sort-by '{.spec.nodeName}'"
+  alias kgpasort="kgpa --sort-by '{.spec.nodeName}'"
+
+  alias kgpwsort="kgpw --sort-by '{.spec.nodeName}'"
+  alias kgpwasort="kgpwa --sort-by '{.spec.nodeName}'"
 
 
   #-----------------------
@@ -192,6 +250,7 @@ create_std_k8s_aliases() {
   alias certs="kubectl get Issuers,ClusterIssuers,Certificates,CertificateRequests,Orders,Challenges"
   alias contexts="kubectl config get-contexts | tail -n +2 | awk '{print \$2}'"
   alias podcidr="kubectl get nodes -o jsonpath='{.items[*].spec.podCIDR}'"
+
 
   #-----------------------
   # K8s Tools
