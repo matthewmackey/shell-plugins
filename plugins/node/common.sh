6364bb8b9436f5876210400e9d4a6e7d4106d85a
Matthew Machaj <21043873+matthewmackey@users.noreply.github.com>
2022-08-22 16:32:59 -0400

    node: add initial node/npm aliases

---
+++ plugins/node/common.sh
@@ -0,0 +1,47 @@
+#------------------------------------------------------------------------------#
+# [npm:aliases] {{{                                                           #
+#------------------------------------------------------------------------------#
+
+  # audit
+  alias npau="npm audit"
+  alias npaulev="npm audit --audit-level="
+  alias npaucrit="npm audit --audit-level=critical"
+  alias npauhigh="npm audit --audit-level=high"
+  alias npauhmod="npm audit --audit-level=moderate"
+
+  # ci
+  alias npci="npm ci"
+
+  # install
+  alias npi="npm install"
+  alias npig="npm install -g"
+
+  # list
+  alias npld="npm list"
+  alias npl="npm list --depth 0"
+  alias nplg="npm list -g --depth 0"
+
+
+  # run
+  alias npr="npm run"
+
+
+  # start
+  alias nps="npm start"
+
+  # test
+  alias npt="npm test"
+  alias npti="npm run test:integration"
+
+
+# }}} npm:aliases
+
+
+#------------------------------------------------------------------------------#
+# [node:functions] {{{                                                         #
+#------------------------------------------------------------------------------#
+
+# }}} node:functions
+
+
+# vim: set ft=sh foldmarker={{{,}}} foldmethod=marker foldlevel=0 :
