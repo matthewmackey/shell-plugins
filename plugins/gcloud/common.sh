01eb682e6b5db5c233afb1fed54ea9f1ab00bb7f
Matthew Machaj <21043873+matthewmackey@users.noreply.github.com>
2022-08-22 16:32:41 -0400

    gcloud: add initial 'gcloud' aliases

---
+++ plugins/gcloud/common.sh
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# [aliases]] {{{
+#-------------------------------------------------------------------------------
+
+  # gcloud
+  alias gc="gcloud"
+
+  # artifacts
+  alias gca="gcloud artifacts"
+  alias gcad="gcloud artifacts docker"
+  alias gcadi="gcloud artifacts docker images"
+  alias gcadt="gcloud artifacts docker tags list"
+  alias gcadtd="gcloud artifacts docker tags delete"
+
+# }}}  aliases
+
+
+# vim: set ft=sh foldmarker={{{,}}} foldmethod=marker foldlevel=100 :
